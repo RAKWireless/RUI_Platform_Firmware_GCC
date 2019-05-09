38fb015c43264cfcc34171af401db21af67bda06
nero <nero.du@rakwireless.com>
2019-05-09 17:08:31 +0800

    lorawan resend bug in os fix

--- RUI/Source/include/FreeRTOSConfig.h
+++ RUI/Source/include/FreeRTOSConfig.h
@@ -95,8 +95,8 @@
 /* Software timer definitions. */
 #define configUSE_TIMERS                                                          1
 #define configTIMER_TASK_PRIORITY                                                 ( 2 )
-#define configTIMER_QUEUE_LENGTH                                                  32
-#define configTIMER_TASK_STACK_DEPTH                                              ( 128 )
+#define configTIMER_QUEUE_LENGTH                                                  16
+#define configTIMER_TASK_STACK_DEPTH                                              ( 256 )
 
 /* Tickless Idle configuration. */
 #define configEXPECTED_IDLE_TIME_BEFORE_SLEEP                                     2
